3047a2cdae39ff99253897d4dc7d33c47941a8c4
Safarov <elkhan.safarov10@yandex.com>
2020-04-12 15:40:38 +0500

    Роутинг

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import './app.css';
-import { withBookstoreService } from '../hoc';
+import { Route, Switch } from 'react-router-dom';
+import { HomePage, CartPage } from '../pages';
 
-const App = ({bookstoreService}) => {
-    return <div>App</div>
+import './app.css';
+
+const App = () => {
+    return (
+        <Switch>
+            <Route
+                path="/"
+                component={HomePage}
+                exact />
+            <Route
+                path="/cart"
+                component={CartPage}
+                />
+        </Switch>
+    )
 }
-export default withBookstoreService()(App);
+export default App;
