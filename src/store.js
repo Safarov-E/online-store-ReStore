1b3347d0186daf35dcb1d6c09dd29a05167a9778
Safarov <elkhan.safarov10@yandex.com>
2020-04-14 21:37:13 +0500

    Store Enhancers

--- src/store.js
+++ src/store.js
@@ -1,7 +1,31 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const LogEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        return originalDispatch(action);
+    }
+    return store;
+}
 
+const stringEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if(typeof action === 'string') {
+            originalDispatch({
+                type: action
+            })
+        }
+        return originalDispatch(action);
+    }
+    return store;
+}
+
+const store = createStore(reducer, compose(stringEnhancer, LogEnhancer));
+
+store.dispatch('HELLO_WORLD');
 export default store;
